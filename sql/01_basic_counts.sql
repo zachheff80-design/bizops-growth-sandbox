d00a428fb8f55aa245e1f08caacc2729f5993cd3
zachheff80-design <zachheff80@gmail.com>
2025-12-21 09:35:52 -0700

    Add basic counts for orders, users, and products

--- sql/01_basic_counts.sql
+++ sql/01_basic_counts.sql
@@ -1,8 +1,23 @@
 -- 01_basic_counts.sql
 -- Goal: Basic counts to understand the dataset
-
-SELECT COUNT(*) AS total_orders
-FROM `bigquery-public-data.thelook_ecommerce.orders`;
-
-SELECT COUNT(*) AS total_users
-FROM `bigquery-public-data.thelook_ecommerce.users`;
+--Questions:
+-- 1) How many orders exist?
+SELECT 
+  COUNT(*) AS total_orders
+FROM 
+  `bigquery-public-data.thelook_ecommerce.orders`;
+-- 2) How many users exist?
+SELECT 
+  COUNT(*) AS total_users
+FROM 
+  `bigquery-public-data.thelook_ecommerce.users`;
+-- 3) How many distinct users exist?
+SELECT
+  COUNT(DISTINCT id) AS distinct_users
+FROM
+  `bigquery-public-data.thelook_ecommerce.users`;
+-- 4) How many products exist?
+SELECT 
+  COUNT(*) AS total_products
+FROM 
+  `bigquery-public-data.thelook_ecommerce.products`;
