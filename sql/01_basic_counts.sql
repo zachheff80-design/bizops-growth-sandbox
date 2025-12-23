9f5130284889318bc28733a58198338ef56f89a4
zachheff80-design <zachheff80@gmail.com>
2025-12-22 21:02:59 -0700

    Update 01_basic_counts.sql

--- sql/01_basic_counts.sql
+++ sql/01_basic_counts.sql
@@ -24,3 +24,33 @@ FROM
 
 --Goal - Write 5 Queries that pull revenue by location. 
 --Use a variety of the following commands: AND, OR, DISTINCT, WHERE, LIMIT
+--1)How many total orders exist in the dataset?
+SELECT
+  COUNT(*)
+FROM
+  `bigquery-public-data.thelook_ecommerce.orders`;
+--2)How many orders exist in each order status (completed, shipped, processing, cancelled, etc.)?
+SELECT 
+  status,
+  COUNT(*) AS count_status
+FROM
+  `bigquery-public-data.thelook_ecommerce.orders`;
+GROUP BY
+  status;
+--3)How many unique customers have placed an order?
+SELECT 
+  COUNT(DISTINCT user_id) AS distinct_users
+FROM
+  `bigquery-public-data.thelook_ecommerce.orders`;
+--4) How many unique products have ever been sold?
+SELECT 
+  COUNT(DISTINCT product_id) AS distinct_products
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items`;
+--5)For each product, how many distinct prices has it been sold at?
+SELECT 
+  COUNT(DISTINCT sale_price) AS distinct_sale_price
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items`
+GROUP BY
+  product_id;
