eec2d79c4720709c741423fad5111071651163da
zachheff80-design <zachheff80@gmail.com>
2025-12-27 18:53:13 -0700

    Add product-level revenue, volume, and average price aggregations

--- sql/02_aggregations.sql
+++ sql/02_aggregations.sql
@@ -1,4 +1,5 @@
 -- Aggregation queries coming next
+--GOAL: Aggregate revenue and volume by site.
 --1) How many total orders exist?
 SELECT 
 COUNT(DISTINCT order_id) AS total_orders
@@ -11,4 +12,34 @@ FROM `bigquery-public-data.thelook_ecommerce.order_items`
 ORDER BY 
   product_id,
   sale_price;
---3)
+--3)What are the top 5 revenue-generating products?
+SELECT
+  product_id,
+  SUM (sale_price) AS total_revenue
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items`
+GROUP BY
+  product_id
+ORDER BY
+  total_revenue DESC
+LIMIT 5
+--4)For each product, how many items were sold and how much total revenue did that product generate?
+SELECT
+  product_id,
+  COUNT(*) AS quantity_sold,
+  SUM (sale_price) AS total_revenue
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items`
+GROUP BY
+  product_id
+--5)What are the top 10 most returned products?
+SELECT
+  product_id,
+  COUNTIF(status = "Returned") AS items_returned
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items`
+GROUP BY
+  product_id
+ORDER BY
+  items_returned DESC
+LIMIT 10
